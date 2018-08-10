724fbb258532dfd83844cb016f4ab025026c2d36
= <13641291123@126.com>
2018-08-10 15:19:05 +0800

    add config file

---
+++ config.py
@@ -0,0 +1,21 @@
+import redis
+
+
+class Config(object):
+    """工程配置信息"""
+    DEBUG = True
+    SECRET_KEY = 'jpNVSNQTyGi1VvWECj9TvC/+kq3oujee2kTfQUs8yCM6xX9Y'
+
+    """数据库相关配置"""
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://admin:mysql@192.168.31.157:3306/infomation'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    """REDIS相关配置"""
+    REDIS_HOST = '192.168.31.157'
+    REDIS_PORT = '6379'
+
+    """flask session 的配置信息"""
+    SESSION_TYPE = 'redis'
+    SESSION_USE_SIGNER = True
+    SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST)
+    PREMANENT_SESSION_LIFETIME = 86400
--- manage.py
+++ manage.py
@@ -1,10 +1,29 @@
 from flask import Flask
+from flask_session import Session
+from flask_sqlalchemy import SQLAlchemy
+import pymysql
+import redis
+from flask_wtf.csrf import CSRFProtect
+from flask_script import Manager
+from flask_migrate import Migrate,MigrateCommand
+from config import Config
 
 app = Flask(__name__)
+
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+redis_store = redis.StrictRedis(host=Config.REDIS_HOST,port=Config.REDIS_PORT)
+CSRFProtect(app)
+Session(app)
+manager = Manager(app)
+Migrate(app,db)
+manager.add_command('db',MigrateCommand)
+
 
 @app.route('/')
 def index():
     return 'index'
 
+
 if __name__ == '__main__':
-    app.run()
+    manager.run()
